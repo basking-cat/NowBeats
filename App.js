b33f8a8fe0317eed4c117cd9ef6c5defb685a12b
Rio <9rio10089@gmail.com>
2025-06-28 19:51:05 +0900

    feat: #9 implement Spotify login and store user info to Firestore

--- App.js
+++ App.js
@@ -1,22 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Linking } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./screens/LoginScreen";
 import FeedScreen from "./screens/FeedScreen";
 import CalendarScreen from "./screens/CalendarScreen";
 import CallbackScreen from "./screens/CallbackScreen";
+import { createNavigationContainerRef } from "@react-navigation/native";
 
-const Stack = createStackNavigator();
+// 現時点ではApp.js内でしか使っていないが、将来的に外部からの画面遷移で使う可能性があるためexport
+export const navigationRef = createNavigationContainerRef();
 
 export default function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const token = await AsyncStorage.getItem("spotifyAccessToken");
+      if (token) setIsLoggedIn(true);
+    };
+    checkLogin();
+  }, []);
+
+  useEffect(() => {
+    const handleUrl = async (e) => {
+      url_parsed = new URL(e.url);
+
+      // ログイン時遷移
+      if (url_parsed.hostname == "callback") {
+        const access_token = url_parsed.searchParams.get("access_token");
+        const refresh_token = url_parsed.searchParams.get("refresh_token");
+        await AsyncStorage.setItem("spotifyAccessToken", access_token);
+        await AsyncStorage.setItem("spotifyRefreshToken", refresh_token);
+      }
+      navigationRef.navigate("Feed");
+    };
+    const subscription = Linking.addEventListener("url", handleUrl);
+
+    return () => {
+      subscription.remove();
+    };
+  });
+
+  const AuthStack = createStackNavigator();
+  function AuthStackScreen() {
+    return (
+      <AuthStack.Navigator initialRouteName="Login">
+        <AuthStack.Screen name="Login" component={LoginScreen} />
+        <AuthStack.Screen name="Callback" component={CallbackScreen} />
+      </AuthStack.Navigator>
+    );
+  }
+
+  const MainStack = createStackNavigator();
+  function MainStackScreen() {
+    return (
+      <MainStack.Navigator initialRouteName="Feed">
+        <MainStack.Screen name="Calendar" component={CalendarScreen} />
+        <MainStack.Screen name="Feed" component={FeedScreen} />
+      </MainStack.Navigator>
+    );
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Feed" component={FeedScreen} />
-        <Stack.Screen name="Calendar" component={CalendarScreen} />
-        <Stack.Screen name="Callback" component={CallbackScreen} />
-      </Stack.Navigator>
+    <NavigationContainer ref={navigationRef}>
+      {isLoggedIn ? <MainStackScreen /> : <AuthStackScreen />}
     </NavigationContainer>
   );
 }
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const cors = require("cors");
-
 const express = require("express");
 const axios = require("axios");
 const app = express();
@@ -9,12 +8,6 @@ require("dotenv").config();
 const clientId = process.env.EXPO_PUBLIC_CLIENT_ID;
 const clientSecret = process.env.EXPO_PUBLIC_CLIENT_SECRET;
 const api_host = process.env.EXPO_API_HOST;
-// デバッグログを追加
-console.log("環境変数の確認:");
-console.log("CLIENT_ID:", clientId);
-console.log("CLIENT_SECRET:", clientSecret);
-console.log("API HOST:", api_host);
-console.log("現在の作業ディレクトリ:", process.cwd());
 
 var admin = require("firebase-admin");
 const serviceAccount = JSON.parse(process.env.EXPO_FIREBASE_ADMIN_KEY);
@@ -23,10 +16,11 @@ admin.initializeApp({
 });
 const db = admin.firestore();
 
+// Spotifyにも同じURLを登録する↓
 const redirectUri = `http://${api_host}:${port}/callback`;
 
 app.get("/login", (req, res) => {
-  const scope = "user-read-currently-playing";
+  const scope = "user-read-email user-read-currently-playing";
   console.log("process.env.CLIENT_ID:", clientId);
   if (clientId === undefined) {
     console.error("cliendId is undefined");
@@ -46,7 +40,7 @@ app.get("/login", (req, res) => {
   }
 });
 
-//login→callback周りの挙動理解する
+// なんかのAPIへgetリクエスト送る時ちゃんと動いてるかブラウザで確認するあれと同じで、ここでres.jsonするとレスポンスがsafariに表示されちゃう
 app.get("/callback", async (req, res) => {
   const code = req.query.code;
   try {
@@ -67,22 +61,74 @@ app.get("/callback", async (req, res) => {
         },
       }
     );
-    res.json(response.data);
+    const res_data = response.data;
+    const access_token = res_data.access_token;
+    const refresh_token = res_data.refresh_token;
+
+    // ユーザ登録
+    const get_user_info_url = `http://${api_host}:${port}/user-info`;
+    const userinfo_res = await axios.get(`${get_user_info_url}`, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    const user_info = userinfo_res.data;
+    const user_doc = db.collection("users").doc(user_info.id);
+    const doc_snapshot = await user_doc.get();
+    if (!doc_snapshot.exists) {
+      await db
+        .collection("users")
+        .doc(user_info.id)
+        .set({
+          username: user_info.display_name,
+          spotifyId: user_info.id,
+          email: user_info.email ?? "",
+          bio: "",
+          friends: [],
+          calendar: {},
+          notifications: [],
+          createdAt: admin.firestore.FieldValue.serverTimestamp(),
+        });
+    }
+
+    // アプリにディープリンク
+    res.redirect(
+      `nowbeats://callback?access_token=${access_token}&refresh_token=${refresh_token}`
+    );
   } catch (e) {
     console.error(e);
     res.status(500).json({ error: "Failed to get token" });
   }
 });
 
+app.get("/user-info", async (req, res) => {
+  const access_token = req.headers.authorization.split(" ")[1];
+
+  try {
+    const response = await axios.get("https://api.spotify.com/v1/me", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    const user_info = response.data;
+    console.log("[debug] user info: ", user_info);
+    res.json(user_info);
+  } catch (e) {
+    console.error("[ERROR] GET /user-info: ", e.message);
+    res.status(500).json({ error: "Failed to get user info" });
+  }
+});
+
 app.get("/current-track", async (req, res) => {
-  const accessToken = req.query.accessToken;
+  const access_token = req.query.access_token;
 
   try {
     const response = await axios.get(
       "https://api.spotify.com/v1/me/player/currently-playing",
       {
         headers: {
-          Authorization: `Bearer ${accessToken}`, //意味がわからない
+          Authorization: `Bearer ${access_token}`,
         },
       }
     );
